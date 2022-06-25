55e6237bfa47a941da36825dce6847e8382fecde
tamaki8021 <hbk2002320@gmail.com>
2022-06-25 20:46:24 +0900

    fix: カレンとディレクトリの処理の削除

--- utils/convert.go
+++ utils/convert.go
@@ -10,7 +10,7 @@ func PrintAAFromTxt(fp string) {
 
 	p, _ := os.Getwd()
 	fmt.Println(p)
-	b, err := os.ReadFile(filepath.Join(p, "aa", fp))
+	b, err := os.ReadFile(filepath.Join("aa", fp))
 	if err != nil {
 		fmt.Println("read file")
 		fmt.Println(err)
@@ -19,10 +19,7 @@ func PrintAAFromTxt(fp string) {
 }
 
 func AAFromText(fp string) string {
-
-	p, _ := os.Getwd()
-	fmt.Println(p)
-	b, err := os.ReadFile(filepath.Join(p, "aa", fp))
+	b, err := os.ReadFile(filepath.Join("aa", fp))
 	if err != nil {
 		fmt.Println(err)
 	}
